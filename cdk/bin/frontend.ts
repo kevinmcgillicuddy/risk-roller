82aefa0418af1a6d9e36cb77856efa67143909bb
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2022-04-15 19:30:03 -0400

    fix stack name

--- cdk/bin/frontend.ts
+++ cdk/bin/frontend.ts
@@ -2,7 +2,7 @@ import { App } from 'aws-cdk-lib';
 import { RiskRollerStack } from '../lib/frontend-stack';
 
 const app = new App();
-let riskRollerStack = new RiskRollerStack(app, 'riskRollerStack', {
+let riskRollerStack = new RiskRollerStack(app, 'frontend-riskRoller', {
   env: {
     account: 'AWS_ACCOUNT',
     region: 'us-east-1'
