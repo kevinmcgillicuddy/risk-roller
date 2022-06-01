618f2e121fe226c64c701fff3786a88232ad9e18
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2022-06-01 19:58:09 -0400

    update test

--- src/app/services/dice.service.spec.ts
+++ src/app/services/dice.service.spec.ts
@@ -17,56 +17,72 @@ describe('DiceService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('roll dice should return a number of arrays', () => {
-    let dice = service.rollDiceResult(2)
-    expect(dice.length).toBeGreaterThan(0);
+  describe('rollDiceResult() ', () => {
+    it('should return a number of arrays', () => {
+      let dice = service.rollDiceResult(2)
+      expect(dice.length).toBeGreaterThan(0);
+    });
   });
 
-  it('maxAttackDice a number', () => {
-    let attackDice = service.maxAttackDice(10, 2);
-    expect(attackDice).toBeGreaterThan(0);
+  describe('maxAttackDice() ', () => {
+    it('should return a number', () => {
+      let attackDice = service.maxAttackDice(attackingArmies, 2);
+      expect(attackDice).toBeGreaterThan(0);
+    });
+
+    it('should return a valid number', () => {
+      let attackDice = service.maxAttackDice(2, 3);
+      expect(attackDice).toBe(1);
+    });
+
+    it('should return 0 when armies is 1', () => {
+      let attackDice = service.maxAttackDice(1, 3);
+      expect(attackDice).toBe(0);
+    });
   });
 
-  it('should have attack return an observable', () => {
-    let result: IResult;
-    service.attack({
-      attackingArmies,
-      defendingArmies,
-      attackingDice: service.maxAttackDice(attackingArmies, 3),
-      attackStop: 3
-    }).subscribe(res => {
-      result = res
-      expect(result).not.toBeNull();
-      expect(result.roll.length).not.toBeNull();
+  describe('attack() ', () => {
+    it('should have attack return an observable', () => {
+      let result: IResult;
+      service.attack({
+        attackingArmies,
+        defendingArmies,
+        attackingDice: service.maxAttackDice(attackingArmies, 3),
+        attackStop: 3
+      }).subscribe(res => {
+        result = res
+        expect(result).not.toBeNull();
+        expect(result.roll.length).not.toBeNull();
+      })
     })
-  })
 
-  it('should have attack return valid remaining armies', () => {
-    let result: IResult;
-    service.attack({
-      attackingArmies,
-      defendingArmies,
-      attackingDice: service.maxAttackDice(attackingArmies, 3),
-      attackStop: 5
-    }).subscribe(res => {
-      result = res
-      expect(result.remaining?.attacker).toBeGreaterThanOrEqual(0);
-      expect(result.remaining?.defender).toBeGreaterThanOrEqual(0);
+    it('should have attack return valid remaining armies', () => {
+      let result: IResult;
+      service.attack({
+        attackingArmies,
+        defendingArmies,
+        attackingDice: service.maxAttackDice(attackingArmies, 3),
+        attackStop: 5
+      }).subscribe(res => {
+        result = res
+        expect(result.remaining?.attacker).toBeGreaterThanOrEqual(0);
+        expect(result.remaining?.defender).toBeGreaterThanOrEqual(0);
+      })
     })
-  })
 
-  it('should have attack return valid remaining armies per each roll', () => {
-    let result: IResult;
-    service.attack({
-      attackingArmies,
-      defendingArmies,
-      attackingDice: service.maxAttackDice(attackingArmies, 3),
-      attackStop: 1
-    }).subscribe(res => {
-      result = res
-      expect(result.roll.find(e => e.attackingArmiesLeft < 0)).toBeUndefined();
-      expect(result.roll.find(e => e.defendingArmiesLeft < 0)).toBeUndefined();
+    it('should have attack return valid remaining armies per each roll', () => {
+      let result: IResult;
+      service.attack({
+        attackingArmies,
+        defendingArmies,
+        attackingDice: service.maxAttackDice(attackingArmies, 3),
+        attackStop: 1
+      }).subscribe(res => {
+        result = res
+        expect(result.roll.find(e => e.attackingArmiesLeft < 0)).toBeUndefined();
+        expect(result.roll.find(e => e.defendingArmiesLeft < 0)).toBeUndefined();
+      })
     })
-  })
+  });
 
 });
