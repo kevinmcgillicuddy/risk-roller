1a74407bcf46bb9214333350633d8c57fcba8556
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2023-11-18 16:14:58 -0500

    add tech

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -11,8 +11,15 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { AppComponent } from './app.component';
+import { DiceService } from './services/dice.service';
+import { of } from 'rxjs';
+import { spyOn } from 'jasmine';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let diceService: DiceService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -30,7 +37,7 @@ describe('AppComponent', () => {
         MatButtonModule,
         MatFormFieldModule,
       ],
-      providers: [FormBuilder]
+      providers: [FormBuilder, DiceService]
     }).compileComponents();
   });
 
@@ -40,4 +47,46 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it('should initialize the form groups and controls', () => {
+    expect(component.attackingCountryGroup).toBeDefined();
+    expect(component.defendingCountryGroup).toBeDefined();
+    expect(component.attackingArmies).toBeDefined();
+  });
+
+  it('should set the initial values for form controls', () => {
+    expect(component.attackingCountryGroup.get('leaveBehind')?.value).toBe(1);
+  });
+
+  it('should calculate attacking dice based on attackingArmies', () => {
+    component.attackingCountryGroup.get('attackingArmies')?.setValue(2);
+    expect(component.attackingDice$).toEqual(of([1]));
+
+    component.attackingCountryGroup.get('attackingArmies')?.setValue(3);
+    expect(component.attackingDice$).toEqual(of([1, 2]));
+
+    component.attackingCountryGroup.get('attackingArmies')?.setValue(4);
+    expect(component.attackingDice$).toEqual(of([1, 2, 3]));
+  });
+
+  it('should call diceService.attack on submit', () => {
+    const mockResult = {
+      winner: '⚔️ Attacker',
+      roll: [],
+      remaining: {
+        attacker: 5,
+        defender: 3,
+      },
+    };
+    spyOn(diceService, 'attack').and.returnValue(of(mockResult));
+
+    component.attackingCountryGroup.get('attackingArmies')?.setValue(5);
+    component.defendingCountryGroup.get('defendingArmies')?.setValue(3);
+    component.attackingCountryGroup.get('attackingDice')?.setValue(3);
+    component.attackingCountryGroup.get('leaveBehind')?.setValue(1);
+
+    component.submit();
+
+    expect(diceService.attack).toHaveBeenCalled();
+    expect(component.result$).toEqual(of(mockResult));
+  });
 });
