b29e5d10f8bac5717c8c381553dac3da8777a5c2
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2023-11-19 14:25:07 -0500

    build fix

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,11 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/risk-roller"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "lcovonly" },
+      ],
     },
     reporters: ["progress", "kjhtml"],
     browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
