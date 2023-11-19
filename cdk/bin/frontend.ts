ddbaffbd652b4062bb7982ad092a22977b8879a9
kevinmcgillicuddy <kevin.mcgillicuddy@outlook.com>
2023-11-19 15:21:06 -0500

    hard-code

--- cdk/bin/frontend.ts
+++ cdk/bin/frontend.ts
@@ -3,12 +3,11 @@ import { RiskRollerStack } from '../lib/frontend-stack';
 
 const app = new App();
 const AWS_ACCOUNT = process.env.AWS_ACCOUNT as string;
-const DOMAIN_NAME = process.env.DOMAIN_NAME as string;
 new RiskRollerStack(app, 'frontend-riskRoller', {
   env: {
     account: AWS_ACCOUNT,
     region: 'us-east-1'
   },
-  domainNames: [...DOMAIN_NAME],
+  domainNames: ['risk-roller.mcgillicuddy.dev'],
   deploymentSource: '../dist/'
 });
